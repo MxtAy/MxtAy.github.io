acf4dfddf6b8e6ab008c3353a4ff3650b8775e4d
MxtAy <mxtay1130@gmail.com>
2025-06-19 16:03:18 +0800

    清空居委会

---
+++ yeelok/js/draw_wordcloud.js
@@ -0,0 +1,144 @@
+list = [];
+var label = 
+    [{ "word": "酸", "freq": 3 },
+     { "word": "你们都是疯子吗", "freq": 7 }, 
+     { "word": "我前男友", "freq": 35 }, 
+     { "word": "卫校", "freq": 2 },
+     { "word": "操行", "freq": 2 }, 
+     { "word": "我也是人", "freq": 2 }, 
+     { "word": "aka", "freq": 5 }, 
+     { "word": "骆驼", "freq": 2 },
+     { "word": "去厕所", "freq": 2 },
+     { "word": "儿子睡觉呢", "freq": 3 }, 
+     { "word": "他挺猛", "freq": 2 }, 
+     { "word": "我就这么操行", "freq": 2 },
+     { "word": "是有点大", "freq": 9 },
+     { "word": "但是想尝试下", "freq": 2 },
+     { "word": "婆婆说我浪费", "freq": 2 },
+      { "word": "把我打晕了", "freq": 2 },
+      { "word": "我来考考你", "freq": 2 },
+      { "word": "肉不行", "freq": 2 },
+      { "word": "最近三天都不会吃烤串了", "freq": 2 },
+      { "word": "主要音乐是我的全部", "freq": 2 },
+    
+     { "word": "酸", "freq": 3 },
+     { "word": "你们都是疯子吗", "freq": 7 }, 
+     { "word": "我前男友", "freq": 35 }, 
+     { "word": "卫校", "freq": 2 },
+     { "word": "操行", "freq": 2 }, 
+     { "word": "我也是人", "freq": 2 }, 
+     { "word": "aka", "freq": 5 }, 
+     { "word": "骆驼", "freq": 2 },
+     { "word": "去厕所", "freq": 2 },
+     { "word": "儿子睡觉呢", "freq": 3 }, 
+     { "word": "他挺猛", "freq": 2 }, 
+     { "word": "我就这么操行", "freq": 2 },
+     { "word": "是有点大", "freq": 9 },
+     { "word": "但是想尝试下", "freq": 2 },
+     { "word": "婆婆说我浪费", "freq": 2 },
+      { "word": "把我打晕了", "freq": 2 },
+      { "word": "我来考考你", "freq": 2 },
+      { "word": "肉不行", "freq": 2 },
+      { "word": "最近三天都不会吃烤串了", "freq": 2 },
+      { "word": "主要音乐是我的全部", "freq": 2 },
+     { "word": "AKA小孔丞相", "freq": 2 },
+	 { "word": "aka库银", "freq": 2 },
+	 { "word": "AKA-老八秘制小馒头", "freq": 2 },
+	 { "word": "AKA鸡蛋", "freq": 2 },
+	 { "word": "AKA库兵欧尼", "freq": 2 },
+	 { "word": "AKA库兵二十一姐", "freq": 2 },
+	 { "word": "AKA_群里一佛", "freq": 2 },
+	 
+  
+           
+      
+
+	 { "word": "AKA-Q", "freq": 2 },
+	 { "word": "AKA 二姑的超能勇士", "freq": 2 },
+	 { "word": "AKA够一梦的", "freq": 2 },
+	 { "word": "AKA炸炸", "freq": 2 },
+	 { "word": "AKA脑瓜子跟着风扇转～～", "freq": 2 },
+	 { "word": "AKA饭盒", "freq": 2 },
+	 { "word": "AKA木雕", "freq": 2 },
+	 { "word": "aka28岁小登", "freq": 2 },
+	 { "word": "AKA 不儿", "freq": 2 },
+	 { "word": "AKA给了", "freq": 2 },
+	 { "word": "AKA山鸟", "freq": 2 },
+	 { "word": "AKA怒掏上牙膛", "freq": 2 },
+	 { "word": "AKA阿达补习班", "freq": 2 },
+	 { "word": "AKA拾柳", "freq": 2 },
+	 { "word": "AKA睡觉小助手", "freq": 2 },
+	 { "word": "AKA穗穗", "freq": 2 },
+	 { "word": "AKA类风湿", "freq": 2 },
+	 { "word": "AKA张志远瓜皮帽", "freq": 2 },
+	 { "word": "AKA小丫头我", "freq": 2 },
+	 { "word": "AKA-王安全", "freq": 2 },
+	 
+	 
+	 { "word": "AKA熊吉吉", "freq": 2 },
+	 { "word": "AKA黑子欣欣", "freq": 2 },
+	 { "word": "AKA我不是黄牛", "freq": 2 },
+	 { "word": "AKA四季", "freq": 2 },
+	 { "word": "AKA 小韭菜", "freq": 2 },
+	 { "word": "AKA达了个达", "freq": 2 },
+	 { "word": "aka超级宝宝", "freq": 2 },
+	 { "word": "aka only like girls", "freq": 2 },
+	 { "word": "aka崽崽", "freq": 2 },
+	 { "word": "AKA哪片云彩有雨", "freq": 2 },
+	 { "word": "AKA鸭鸭泰国牌", "freq": 2 },
+	 { "word": "AKA炸带鱼", "freq": 2 },
+	 { "word": "aka艺术家俯首称臣", "freq": 2 },
+	 { "word": "AKA 句号", "freq": 2 },
+	 { "word": "AKA吐纳风云", "freq": 2 },
+	 { "word": "AKA蘑菇怪", "freq": 2 },
+	 
+	 
+	 { "word": "AKA 火烧", "freq": 2 },
+	 { "word": "AKA范增的徒弟", "freq": 2 },
+	 { "word": "AKA通胖高速", "freq": 2 },
+	 { "word": "AKA三儿", "freq": 2 },
+	 { "word": "AKA邓姐土豆批发", "freq": 2 },
+	 { "word": "AKA-骷髅帮", "freq": 2 },
+	 { "word": "AKA赵雅芳", "freq": 2 },
+	 { "word": "AKA 黄金办公桌拥有者", "freq": 2 },
+	 { "word": "AKA 我要变成大富翁（延毕版）", "freq": 2 },
+	 { "word": "AKA孤单听雨的猫", "freq": 2 },
+	 { "word": "AKA晓孔丞相", "freq": 2 },
+	 { "word": "AKA 拨雨树", "freq": 2 },
+	 { "word": "AKA粉红贡丸", "freq": 2 },
+	 { "word": "BKB冬瓜", "freq": 2 },
+	 
+    ];
+
+for (var i in label) {
+    if (label[i]["freq"] > 0) {
+        list.push([label[i]["word"], label[i]["freq"]])
+    }
+
+}
+
+var div = document.getElementById("canvas-container")
+
+var canvas = document.getElementById("canvas_cloud");
+
+canvas.height = div.offsetHeight;
+
+canvas.width = div.offsetWidth;
+
+var options = {
+    list: list,
+    gridSize: Math.round(8 * document.getElementById('canvas_cloud').offsetWidth / 1024),
+    weightFactor: function(size) {
+
+        wf = Math.pow(size, 0.3) * 15 * document.getElementById('canvas_cloud').offsetWidth / 1024;
+        console.log(wf);
+        return wf
+        // return 180/max(size);
+    },
+    rotateRatio: 0.5,
+    rotationSteps: 2,
+    color: 'random-light',
+    backgroundColor: "transparent"
+}
+
+WordCloud(document.getElementById('canvas_cloud'), options);
